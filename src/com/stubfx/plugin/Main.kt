f1ef5a36f5b67ea01b4017a6ba19100e4cfed4e6
stubfx <luca.molluso@gmail.com>
2022-01-10 22:16:57 +0100

    add multiple commands from chat + refactor

--- src/com/stubfx/plugin/Main.kt
+++ src/com/stubfx/plugin/Main.kt
@@ -1,96 +1,28 @@
 package com.stubfx.plugin
 
+import com.stubfx.plugin.chatreactor.ChatReactor
 import com.stubfx.plugin.listeners.EntityListener
 import com.stubfx.plugin.listeners.PlayerListener
 import com.stubfx.plugin.listeners.ProjectileListener
-import com.sun.net.httpserver.HttpExchange
-import com.sun.net.httpserver.HttpHandler
-import com.sun.net.httpserver.HttpServer
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
-import org.bukkit.entity.EntityType
-import org.bukkit.entity.Item
-import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitRunnable
-import java.io.IOException
-import java.net.InetSocketAddress
-import java.util.*
 
 
 class Main : JavaPlugin() {
 
-    private val commandCooldown = 2*1000*60 // 2 mins cooldown in millis
-    private var lastCommandTime = 0L
-    private var apyKey = "ERROR"
-    private var httpserver : HttpServer? = null
-    private var httpServerSocket : InetSocketAddress? = null
+    private var chatReactor = ChatReactor(this)
 
-    private fun startServer() {
-        httpServerSocket = InetSocketAddress(8001)
-        httpserver = HttpServer.create(httpServerSocket, 0)
-        httpserver?.createContext("/command", MyHandler(this))
-        httpserver?.executor = null // creates a default executor
-        httpserver?.start()
-    }
-
-    internal class MyHandler(private val main: Main) : HttpHandler {
-        @Throws(IOException::class)
-        override fun handle(t: HttpExchange) {
-            val query = t.requestURI.query
-            var params : HashMap<String, String> = HashMap()
-            query.split("&").forEach {
-                var split = it.split("=")
-                params.put(split[0], split[1])
-            }
-            t.sendResponseHeaders(204, -1)
-            t.responseBody.close()
-            main.chatCommandResolve(params["name"]!!, params["command"]!!)
-        }
-    }
-
-    private fun chatCommandResolve(name: String, command: String) {
+    fun runOnBukkit(func: () -> Unit) {
         object : BukkitRunnable() {
             override fun run() {
-                val date = Date().time
-                if (date < lastCommandTime + commandCooldown){
-                    return
-                }
-                // in this case, COMMAND_COOLDOWN has passed
-                // and we can run the command
-                var hasCommandRun = true
-                when (command.lowercase()) {
-                    "creeper" -> creeperSpawn()
-                    "dropit" -> forceDropItem()
-                    "fire" -> forceDropItem()
-                    else -> {
-                        // in this case the command is not listed above
-                        hasCommandRun = false
-                    }
-                }
-                if (hasCommandRun) {
-                    lastCommandTime = date
-                    // TODO change with title command
-                    server.onlinePlayers.forEach{
-                        it.player?.sendMessage("${name} has run the command ${command}")
-                    }
-                }
+                func()
             }
         }.runTask(this)
     }
 
-    private fun forceDropItem() {
-        server.onlinePlayers.forEach {
-            val item = it.itemInUse ?: return
-            it.inventory.remove(item)
-            val itemDropped: Item = it.world.dropItemNaturally(it.location, item)
-            itemDropped.pickupDelay = 40
-        }
-    }
-
     override fun onEnable() {
-//        apyKey = System.getenv("apiKey")
-        startServer()
 //        getCommand("clearchunk")?.tabCompleter = MaterialTabCompleter()
 //        getCommand("sectionreplace")?.tabCompleter = MaterialTabCompleter()
 //        getCommand("chunkreplace")?.tabCompleter = MaterialTabCompleter()
@@ -104,23 +36,15 @@ class Main : JavaPlugin() {
     }
 
     override fun onDisable() {
-        server.operators.forEach{
-            it.player?.sendMessage("RELOAAAAAAAD")
-        }
-        httpserver?.stop(0)
+        chatReactor.onDisable()
     }
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
-        when (command.name.lowercase()) {
-            "creep" -> creeperSpawn()
-            "dropit" -> forceDropItem()
-        }
+//        when (command.name.lowercase()) {
+//            "creep" -> chatReactor.creeperSpawn()
+//            "dropit" -> chatReactor.forceDropItem()
+//        }
         return false
     }
 
-    private fun creeperSpawn() {
-        server.onlinePlayers.forEach { player ->
-            player.world.spawnEntity(player.location, EntityType.CREEPER)
-        }
-    }
 }
--- src/com/stubfx/plugin/listeners/EntityListener.kt
+++ src/com/stubfx/plugin/listeners/EntityListener.kt
@@ -21,14 +21,14 @@ class EntityListener(val main: Main) : Listener {
 
     @EventHandler
     fun onEntityDamageByEntity(event: EntityDamageByEntityEvent) {
-        if (event.damager.isOp) {
-            // then the attacker is an operator
-            val player = event.damager as Player
-            if (player.inventory.itemInMainHand.type == Material.STICK) {
-                val levitation = PotionEffectType.LEVITATION.createEffect(main.getTicks() * 3, 50)
-                (event.entity as LivingEntity).addPotionEffect(levitation)
-            }
-        }
+//        if (event.damager.isOp) {
+//            // then the attacker is an operator
+//            val player = event.damager as Player
+//            if (player.inventory.itemInMainHand.type == Material.STICK) {
+//                val levitation = PotionEffectType.LEVITATION.createEffect(main.getTicks() * 3, 50)
+//                (event.entity as LivingEntity).addPotionEffect(levitation)
+//            }
+//        }
     }
 
 }
